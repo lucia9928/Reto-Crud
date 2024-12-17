84b5c0ea88fcfc4e37481733f378c9aebc094f28
markel <markelarabiourrutia@gmail.com>
2024-12-17 12:59:25 +0100

    subidaGestionaId

--- src/java/eus/tartangalh/crud/create/GestionaId.java
+++ src/java/eus/tartangalh/crud/create/GestionaId.java
@@ -6,12 +6,52 @@
 package eus.tartangalh.crud.create;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
  *
  * @author 2dam
  */
 public class GestionaId implements Serializable{
+    
+    private String dni;
+    private Integer idProducto;
+    private LocalDate fechaCompra;
+
+    public GestionaId(String dni, Integer idProducto, LocalDate fechaCompra) {
+        this.dni = dni;
+        this.idProducto = idProducto;
+        this.fechaCompra = fechaCompra;
+    }
+
+    public GestionaId() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(LocalDate fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+    
+    
     
     
 }
