d17997f8f4467cfd724b78c4a8ab64448984c216
lucia9928 <144560946+lucia9928@users.noreply.github.com>
2025-01-15 08:36:08 +0100

    Merge origin/entidadRecetaMedica into entidadRecetaMedica
    Conflicts:
    	src/java/eus/tartangalh/crud/create/RecetaMedica.java

--- src/java/eus/tartangalh/crud/ejb/GestionaInterfaz.java
+++ src/java/eus/tartangalh/crud/ejb/GestionaInterfaz.java
@@ -11,15 +11,8 @@ import excepciones.BorrarException;
 import excepciones.CrearException;
 import excepciones.LeerException;
 import java.util.List;
-import java.util.logging.Level;
 import javax.ejb.Local;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.PathSegment;
 
 /**
