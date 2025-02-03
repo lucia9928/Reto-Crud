f027adf5db63536f221d3b1f02324077d967e235
markel <markelarabiourrutia@gmail.com>
2025-02-03 10:20:33 +0100

    Servidor Actualizado

--- src/java/eus/tartangalh/crud/create/Usuario.java
+++ src/java/eus/tartangalh/crud/create/Usuario.java
@@ -9,9 +9,6 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.Transient;
-
 /**
  *
  * @author melany
